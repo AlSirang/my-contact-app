9c7dd660e0b13285ff19c7b9a5a286ce2e8bf7c3
bashir uddin <uddinbashir1999@gmail.com>
2020-12-10 19:49:53 +0500

    Add props to Card component

--- src/contacts.js
+++ src/contacts.js
@@ -1,23 +1,25 @@
 const contacts = [
-  {
-    name: "Beyonce",
-    imgURL:
-      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
-    phone: "+123 456 789",
-    email: "b@beyonce.com"
-  },
-  {
-    name: "Jack Bauer",
-    imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
-  },
-  {
-    name: "Chuck Norris",
-    imgURL:
-      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
-    phone: "+918 372 574",
-    email: "gmail@chucknorris.com"
-  }
+    {
+        name: "Tom",
+        imgURL:
+            "https://mpng.subpng.com/20190506/ilt/kisspng-jerry-mouse-tom-cat-tom-and-jerry-spike-and-tyke-5cd0c82c808e44.8726447015571866045266.jpg",
+        phone: "+123 456 789",
+        email: "tom@tom.com",
+    },
+    {
+        name: "Jerry",
+        imgURL:
+            "https://banner2.cleanpng.com/20180704/xxa/kisspng-jerry-mouse-tom-cat-tom-and-jerry-cartoon-clip-art-tom-cartoon-5b3cdb5f7e1601.4644385715307149755165.jpg",
+        phone: "+987 654 321",
+        email: "jerry@jerry.com",
+    },
+    {
+        name: "Spike",
+        imgURL:
+            "https://mpng.subpng.com/20190616/iiq/kisspng-spike-the-bulldog-and-chester-the-terrier-jerry-mo-tom-and-jerry-clipart-white-background-11-98-x-5d05e13db6d2b9.6358507115606664297489.jpg",
+        phone: "+918 372 574",
+        email: "spike@spike.com",
+    },
 ];
+
+export default contacts;
